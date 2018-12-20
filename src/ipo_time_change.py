baaa91afe79fe90c5a783e53d531fd2b1ead298e
DMJ <1615275193@qq.com>
2018-12-20 17:11:57 +0800

    test

--- src/ipo_time_change.py
+++ src/ipo_time_change.py
@@ -22,63 +22,88 @@ json_file_changed = 'C:\\Users\DMJ\Desktop\工作日常记录\资料\\time_chang
     "t15": "2016 年 1 月 1 日至2017 年 12 月 31 日，协议期满前 30 日内双方如无异议，可自动延期 1 年，顺延次数不限",
     "t16": "合同有效期为 2016 年 1 月 1 日至 2016 年 12 月31 日。《商品合同》约定，任何一方有权在前述合同期限届满至少提前一个月书面通知另一方 于前述期限届满时终止本《商合， 使前述终止权，则本《商品合同》将视为已经由双方默认为无限期续订而有效并对双方具有约束力，在续订期内，任何一方有权提前一个月书面通知另一方以终止本《商品合同》",
 """
+# 时间切片，补零
+def slice_zero_pad(value):
+    if re.search(r'^\d{4}\-\d{1}$', value):
+        value_begin = value[0:5]
+        value_end = value[5:]
+        value = value_begin + '0' + value_end
+    if re.search(r'^\d{4}\-\d{1}\-', value):
+        value_begin = value[0:5]
+        value_end = value[5:]
+        value = value_begin + '0' + value_end
+    if re.search(r'^\d{4}\-\d{2}\-\d{1}$', value):
+        value_begin = value[0:8]
+        value_end = value[8:]
+        value = value_begin + '0' + value_end
+    return value
+
 # 有待优化，不必要所有的判断都得执行
 def time_change(time):
     time = str(time)
     value = time.strip()
-    try:
-        # 2016.12.31
-        if re.search(r'^\d{4}\.\d{1,2}\.\d{1,2}$', value):
-            value = value.replace(".", "-")
+    value = value.replace('\n', '')
 
-        # 2020-05-09 00:00:00
-        if re.search(r'^\d{4}\-\d{1,2}(\-\d{1,2})\s*\d{2}\:\d{2}\:\d{2}$', value):
-            value = value.replace(" 00:00:00", "")
+    # 2016.12.31
+    if re.search(r'^\d{4}\.\d{1,2}\.\d{1,2}$', value):
+        value = value.replace(".", "-")
 
-        # 2016 年 12 月 31 日
-        if re.search(r'^\d{4}\s*[\u4E00-\u9FFF]\s*\d{1,2}\s*[\u4E00-\u9FFF]\s*\d{1,2}\s*[\u4E00-\u9FFF]$', value):
-            value = value.replace("年", "-")
-            value = value.replace("月", "-")
-            value = value.replace("日", "")
-            value = value.replace(" ", "")
+    # 2020-05-09 00:00:00
+    if re.search(r'^\d{4}\-\d{1,2}(\-\d{1,2})\s*\d{2}\:\d{2}\:\d{2}$', value):
+        value = value.replace(" 00:00:00", "")
 
-        # 2016 年 4 月
-        if re.search(r'^\d{4}\s*[\u4E00-\u9FFF]\s*\d{1,2}\s*[\u4E00-\u9FFF]$', value):
-            value = value.replace("年", "-")
-            value = value.replace("月", "")
-            value = value.replace(" ", "")
+    # 2016 年 12 月 31 日
+    if re.search(r'^\d{4}\s*年\s*\d{1,2}\s*月\s*\d{1,2}\s*日$', value):
+        value = value.replace("年", "-")
+        value = value.replace("月", "-")
+        value = value.replace("日", "")
+        value = value.replace(" ", "")
 
-        # 2018 年
-        if re.search(r'^\d{4}\s*[\u4E00-\u9FFF]\s*$', value):
-            value = value.replace("年", "")
+    # 2016 年 4 月
+    if re.search(r'^\d{4}\s*年\s*\d{1,2}\s*月$', value):
+        value = value.replace("年", "-")
+        value = value.replace("月", "")
+        value = value.replace(" ", "")
 
-        # 2017/6/7-2018/5/20
-        if re.search(r'^\d{4}\/\d{1,2}\/\d{1,2}\-\d{4}\/\d{1,2}\/\d{1,2}$', value):
-            value = value.replace("-", "~")
-            value = value.replace("/", "-")
+    # 2018 年
+    if re.search(r'^\d{4}\s*年\s*$', value):
+        value = value.replace("年", "")
 
-        # 2017.4.1-2020.3.31
-        if re.search(r'^\d{4}\.\d{1,2}\.\d{1,2}\-\d{4}\.\d{1,2}\.\d{1,2}$', value):
-            value = value.replace("-", "~")
-            value = value.replace(".", "-")
+    # 2017/6/7-2018/5/20
+    if re.search(r'^\d{4}\/\d{1,2}\/\d{1,2}\-\d{4}\/\d{1,2}\/\d{1,2}$', value):
+        value = value.replace("-", "~")
+        value = value.replace("/", "-")
 
-        # 2017年6月21日至2018年6月21日 / 2015 年 1月 30 日至2024 年 10月 29 日期间发生的债权/2015 年 1 月 1 日起至 2017 年 12 月 31 日
-        if re.search(
-                r'^[\u4E00-\u9FFF]*\s*\d{4}\s*[\u4E00-\u9FFF]\s*\d{1,2}\s*[\u4E00-\u9FFF]\s*\d{1,2}\s*[\u4E00-\u9FFF]+\s*\d{4}\s*'
-                r'[\u4E00-\u9FFF]\s*\d{1,2}\s*[\u4E00-\u9FFF]\s*\d{1,2}\s*[\u4E00-\u9FFF]+', value):
-            value = value.replace("年", "-")
-            value = value.replace("月", "-")
-            value = value.replace("日", "")
-            value = value.replace(" ", "")
-            time_list = re.findall(r'\d{4}\-\d{1,2}\-\d{1,2}', value)
-            value = time_list[0] + '~' + time_list[1]
+    # 2017.4.1-2020.3.31
+    if re.search(r'^\d{4}\.\d{1,2}\.\d{1,2}\-\d{4}\.\d{1,2}\.\d{1,2}$', value):
+        value = value.replace("-", "~")
+        value = value.replace(".", "-")
 
-        # 2018-01-01 至 2018-12-31
-        if re.search(r'^\d{4}\-\d{1,2}\-\d{1,2}\s*[\u4E00-\u9FFF]+\s*\d{4}\-\d{1,2}\-\d{1,2}$', value):
-            time_list = re.findall(r'\d{4}\-\d{1,2}\-\d{1,2}', value)
-            value = time_list[0] + '~' + time_list[1]
-    except Exception as e:
-        print(e)
+    # 2017年6月21日至2018年6月21日 / 2015 年 1月 30 日至2024 年 10月 29 日期间发生的债权/2015 年 1 月 1 日起至 2017 年 12 月 31 日
+    if re.search(r'^[\u4E00-\u9FFF]*\s*\d{4}\s*年\s*\d{1,2}\s*月\s*\d{1,2}\s*[\u4E00-\u9FFF]+\s*\d{4}\s*'
+                 r'年\s*\d{1,2}\s*月\s*\d{1,2}\s*[\u4E00-\u9FFF]+', value):
+        time_list = re.findall(r'\d{4}\s*年\s*\d{1,2}\s*月\s*\d{1,2}', value)
+        value = time_list[0] + '~' + time_list[1]
+        value = value.replace("年", "-")
+        value = value.replace("月", "-")
+        value = value.replace("日", "")
+        value = value.replace(" ","")
+
+    # 2018-01-01 至 2018-12-31
+    if re.search(r'^\d{4}\-\d{1,2}\-\d{1,2}\s*[\u4E00-\u9FFF]+\s*\d{4}\-\d{1,2}\-\d{1,2}$', value):
+        time_list = re.findall(r'\d{4}\-\d{1,2}\-\d{1,2}', value)
+        value = time_list[0] + '~' + time_list[1]
+
+    # 2018/12/1
+    if re.search(r'^\d{4}\/\d{1,2}\/\d{1,2}$', value):
+        value = value.replace('/', '-')
+
+    # 2016.12.16至2017.12.15
+    if re.search(r'^\d{4}\.\d{1,2}\.\d{1,2}\s*[\u4E00-\u9FFF]+\s*\d{4}\.\d{1,2}\.\d{1,2}$', value):
+        time_list = re.findall(r'\d{4}\.\d{1,2}\.\d{1,2}', value)
+        value = time_list[0] + '~' + time_list[1]
+        value = value.replace('.', '-')
+
 
     return value
 
@@ -131,18 +156,23 @@ def json_time_change(dic_data):
     # 主要客户
     if dic_data['主要客户'] is not None:
         dic_data = time_change_common(dic_data,'主要客户', '时间')
-        # 主要供应商
+    # 主要供应商
     if dic_data['主要供应商'] is not None:
         dic_data = time_change_common(dic_data,'主要供应商', '时间')
-        # 重大合同
+    # 重大合同
     if dic_data['重大合同'] is not None:
         dic_data = time_change_common(dic_data, '重大合同', '履行期限')
-        # 盈利能力
+    # 盈利能力
     if dic_data['盈利能力'] is not None:
         dic_data = time_change_common(dic_data, '盈利能力', '报表日期')
-        # 财务基本情况及财务指标
+    # 财务基本情况及财务指标
     if dic_data['财务基本情况及财务指标'] is not None:
         dic_data = time_change_common(dic_data, '财务基本情况及财务指标', '报表日期')
+    # 专利
+    if dic_data['专利'] is not None:
+        dic_data = time_change_common(dic_data, '专利', '取得日期')
+        dic_data = time_change_common(dic_data, '专利', '使用期限')
+    # print(dic_data['专利'])
 
     return dic_data
 
@@ -299,13 +329,14 @@ def amount_change(dic_data):
         for major_contract in dic_data['重大合同']:
             if isinstance(major_contract, dict):
                 for key_first, value_first in major_contract.items():
-                    if re.search(r'^(-?\d+)(\.\d+)?$', str(value_first)):
-                        if re.search(r'^(-?\d+)(\.\d+)$', str(value_first)):
-                            value_first = float(value_first)
-                        else:
-                            value_first = int(value_first)
-                        amount = "{:,}".format(value_first)
-                        major_contract[key_first] = amount
+                    if key_first != '履行期限':
+                        if re.search(r'^(-?\d+)(\.\d+)?$', str(value_first)):
+                            if re.search(r'^(-?\d+)(\.\d+)$', str(value_first)):
+                                value_first = float(value_first)
+                            else:
+                                value_first = int(value_first)
+                            amount = "{:,}".format(value_first)
+                            major_contract[key_first] = amount
         # print(dic_data['重大合同'])
 
     # 主要供应商
@@ -313,13 +344,14 @@ def amount_change(dic_data):
         for major_supplier in dic_data['主要供应商']:
             if isinstance(major_supplier, dict):
                 for key_first, value_first in major_supplier.items():
-                    if re.search(r'^(-?\d+)(\.\d+)?$', str(value_first)):
-                        if re.search(r'^(-?\d+)(\.\d+)$', str(value_first)):
-                            value_first = float(value_first)
-                        else:
-                            value_first = int(value_first)
-                        amount = "{:,}".format(value_first)
-                        major_supplier[key_first] = amount
+                    if key_first != '时间':
+                        if re.search(r'^(-?\d+)(\.\d+)?$', str(value_first)):
+                            if re.search(r'^(-?\d+)(\.\d+)$', str(value_first)):
+                                value_first = float(value_first)
+                            else:
+                                value_first = int(value_first)
+                            amount = "{:,}".format(value_first)
+                            major_supplier[key_first] = amount
         # print(dic_data['主要供应商'])
 
     # 主要客户
@@ -327,13 +359,14 @@ def amount_change(dic_data):
         for major_client in dic_data['主要客户']:
             if isinstance(major_client, dict):
                 for key_first, value_first in major_client.items():
-                    if re.search(r'^(-?\d+)(\.\d+)?$', str(value_first)):
-                        if re.search(r'^(-?\d+)(\.\d+)$', str(value_first)):
-                            value_first = float(value_first)
-                        else:
-                            value_first = int(value_first)
-                        amount = "{:,}".format(value_first)
-                        major_client[key_first] = amount
+                    if key_first != '时间':
+                        if re.search(r'^(-?\d+)(\.\d+)?$', str(value_first)):
+                            if re.search(r'^(-?\d+)(\.\d+)$', str(value_first)):
+                                value_first = float(value_first)
+                            else:
+                                value_first = int(value_first)
+                            amount = "{:,}".format(value_first)
+                            major_client[key_first] = amount
         # print(dic_data['主要客户'])
 
     # 募集资金与运用
@@ -399,24 +432,26 @@ class FormatChange():
             with open(file_path, encoding='utf-8') as p_file:
                 dic_data = json.load(p_file)
 
-            # 时间格式规整
-            dic_data = json_time_change(dic_data)
-
-            # 境外居住权规整
-            dic_data = overseas_residency_change(dic_data)
-
-            #是否存在权属纠纷
-            dic_data = patent_ownership(dic_data)
-
-            # 性别
-            dic_data = gander_change(dic_data)
+            # # 时间格式规整
+            # dic_data = json_time_change(dic_data)
+            #
+            # # 境外居住权规整
+            # dic_data = overseas_residency_change(dic_data)
+            #
+            # #是否存在权属纠纷
+            # dic_data = patent_ownership(dic_data)
+            #
+            # # 性别
+            # dic_data = gander_change(dic_data)
 
             # 金额
-            dic_data = amount_change(dic_data)
+            # dic_data = amount_change(dic_data)
+
             # print(dic_data['财务基本情况及财务指标'])
 
 
-            json_data = json.dumps(dic_data, ensure_ascii=False)
+            json_data = json.dumps(dic_data, ensure_ascii=False).replace(" NaN", '"无"')
+            # print(json_data)
             file_path_changed = os.path.join(json_file_changed, json_file)
             with open(file_path_changed, 'w', encoding='utf-8') as n_file:
                 n_file.write(json_data)
